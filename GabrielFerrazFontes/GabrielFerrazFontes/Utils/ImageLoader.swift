94483d6c9059278f87fce84461e3626d4fc76182
Gabriel Ferraz Fontes <brielff@hotmail.com>
2020-12-22 11:32:59 -0300

    Utils documented

--- GabrielFerrazFontes/GabrielFerrazFontes/Utils/ImageLoader.swift
+++ GabrielFerrazFontes/GabrielFerrazFontes/Utils/ImageLoader.swift
@@ -8,15 +8,26 @@
 import SwiftUI
 import UIKit
 
+/// Image saved at cache
 private let _imageCache = NSCache<AnyObject, AnyObject>()
 
+/**
+ Class helper to load images based on their url
+ */
 class ImageLoader: ObservableObject {
     
     @Published var image: UIImage?
     @Published var isLoading = false
     
+    /// reference of image cache
     var imageCache = _imageCache
     
+    /**
+     Load a image using their url
+     
+     - parameters:
+        - url: url of image to be loaded
+     */
     func loadImage(with url: URL){
         let urlString = url.absoluteString
         if let imageFromCache = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
@@ -27,6 +38,7 @@ class ImageLoader: ObservableObject {
         DispatchQueue.global(qos: .background).async { [weak self] in
             guard let self = self else { return }
             do{
+                // cast from data to UIImage async
                 let data = try Data(contentsOf: url)
                 guard let image = UIImage(data: data) else { return }
                 self.imageCache.setObject(image, forKey: urlString as AnyObject)
--- GabrielFerrazFontes/GabrielFerrazFontes/Utils/Utils.swift
+++ GabrielFerrazFontes/GabrielFerrazFontes/Utils/Utils.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
+/**
+ Class used as a constants holder
+ */
 class Utils {
+    
+    /// Decoder of JSON
     static let jsonDecoder: JSONDecoder = {
         let jsonDecoder = JSONDecoder()
         jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
@@ -15,6 +20,7 @@ class Utils {
         return jsonDecoder
     }()
     
+    /// Date formatter used "yyyy-mm-dd"
     static let dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-mm-dd"
